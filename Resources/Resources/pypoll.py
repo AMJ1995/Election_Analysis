213a7a1cc74846c6ee00d705c65323fbe4756f0e
Adam Joy <adamjoy@Adams-MBP.attlocal.net>
2022-03-15 22:05:57 -0500

    updated and saved

--- Resources/Resources/pypoll.py
+++ Resources/Resources/pypoll.py
@@ -9,7 +9,28 @@ import csv
 import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     headers = next(file_reader)
-    print(headers)
+    for row in file_reader:
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        print(f"{candidate_name}: {vote_percentage: .1f}% ({votes:,})\n")
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+                winning_count = votes
+                winning_percentage = vote_percentage
+                winning_candidate =  candidate_name
+    print(f"{candidate_name}: received {vote_percentage}% of the vote.")
