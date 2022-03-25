513aed90224a4bd177b77044475b5b2df515574a
Adam Joy <adamjoy@Adams-MacBook-Pro.local>
2022-03-25 13:53:33 -0500

    finished challenge

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -6,9 +6,9 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("/Users/adamjoy/Desktop/Election_Analysis/Resources/Resources/election_results.csv")
+file_to_load = os.path.join("Resources/election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("/Users/adamjoy/Desktop/Election_Analysis/Resources/analysis/election_results.txt")
+file_to_save = os.path.join("analysis/election_results.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
